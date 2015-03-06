14db8a3f8f0c51926f5a072bf4a6d3c4ca39278e
Heshy <jroskes1@jhu.edu>
2015-03-06 13:50:52 -0500

    Don't init the TLorentzVector stuff unless it's needed

--- momentum.py
+++ momentum.py
@@ -9,15 +9,57 @@ def newevent():
 
 class Momentum(ROOT.TLorentzVector):
     def __init__(self, px, py, pz, E):
-        super(Momentum, self).__init__(px, py, pz, E)
+        self.px = px
+        self.py = py
+        self.pz = pz
+        self.e = E
+        self.m = copysign(abs(E**2-px**2-py**2-pz**2)**0.5,E**2-px**2-py**2-pz**2)
+        self.superinited = False
         momenta.append(self)
+
+    def Px(self):
+        return self.px
+    def Py(self):
+        return self.py
+    def Pz(self):
+        return self.pz
+    def E(self):
+        return self.e
+    def M(self):
+        return self.m
+    def P(self):
+        return (self.px**2+self.py**2+self.pz**2)**.5
+
+    def Vect(self):
+        self.superinit()
+        return super(Momentum, self).Vect()
+    def BoostVector(self):
+        self.superinit()
+        return super(Momentum, self).BoostVector()
+    def Boost(self, *args):
+        self.superinit()
+        return super(Momentum, self).Boost(*args)
+    def Rotate(self, *args):
+        self.superinit()
+        return super(Momentum, self).Rotate(*args)
 
     def __eq__(self, other):
         return (self-other).P()**2 + (self-other).E()**2 < config.momentumtolerance
     def __ne__(self, other):
         return not (self == other)
+    def __neg__(self):
+        return Momentum(-self.Px(), -self.Py(), -self.Pz(), -self.E())
+    def __add__(self, other):
+        return Momentum(self.Px()+other.Px(), self.Py()+other.Py(), self.Pz()+other.Pz(), self.E()+other.E())
+    def __sub__(self, other):
+        return self + (-other)
     def __str__(self):
         return "(%s, %s, %s, %s)" % (self.Px(), self.Py(), self.Pz(), self.E())
+
+    def superinit(self):
+        if not self.superinited:
+            super(Momentum, self).__init__(self.Px(), self.Py(), self.Pz(), self.E())
+            self.superinited = True
 
 def boostall(x, y = None, z = None):
     if y is None and z is not None or y is not None and z is None:
